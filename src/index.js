17c3d21f2e2cbbf69b4705c124e0746ea1079a48
wuyepabdul <wuyepabdul@gmail.com>
2021-10-11 22:23:17 +0100

    Add pop window markup

--- src/index.js
+++ src/index.js
@@ -1,53 +1,123 @@
 import './style.css';
-import mealImg from './images/meal.jpg';
+import Image from './space.jpg';
+import icon from './icon.svg';
 
-const createPopup = () => {
-  let popupMarkup = ` 
-  <small class='close-menu'>X</small>   
-  <div class='blur-background'> 
-    
-    <div class="popup-img-div"><img class="meal-popup-img" src="${mealImg}" alt="meal" /></div>
-    <div class="popup-details-div">
-    <h1>Meal title</h1>
-    <div class="details-list">
-        <ul>
-        <li>Category: Rice</li>
-        <li>Category: Rice</li>
-        </ul>
-        <ul>
-        <li>Category: Rice</li>
-        <li>Category: Rice</li>
-        </ul>
-    </div>
-  </div>`;
-
-  const formContainer = `
-  <div class="comments-container">
-  <div class="comments">
-    <h3>Comments (3)</h3>
-  </div>
-  <h3>Add a comment</h3>
-  <form>
-    <div class="input-group">
-      <input type="text" class="nameInput" placeholder="Your name" />
-    </div>
-    <div class="input-group">
-      <textarea
-        name="comment"
-        id="comment"
-        cols="30"
-        rows="10"
-        placeholder="Your insights"
-      ></textarea>
-    </div>
-    <div class="button-div">
-      <button>Comment</button>
-    </div>
-  </form>
-</div>`;
-
-  const popupSection = document.querySelector('.popup-window');
-  popupSection.innerHTML = popupMarkup;
-//   popupSection.appendChild(formContainer);
+const elementGenerator = (typeName, className) => {
+  const element = document.createElement(typeName);
+  if (className) element.className = className;
+  return element;
 };
-createPopup();
+
+const header = elementGenerator('header');
+const logo = elementGenerator('div', 'logo');
+logo.textContent = 'Restaurant logo';
+const navigation = elementGenerator('nav');
+const uList = elementGenerator('ul');
+const listOne = elementGenerator('li', 'meals');
+const linkOne = elementGenerator('a');
+linkOne.href = '#';
+linkOne.textContent = 'Meals(6)';
+const listTwo = elementGenerator('li');
+const linkTwo = elementGenerator('a');
+linkTwo.href = '#';
+linkTwo.textContent = 'Planets';
+
+const listThree = elementGenerator('li');
+const linkThree = elementGenerator('a');
+linkThree.href = '#';
+linkThree.textContent = 'Races';
+
+const footer = elementGenerator('footer');
+footer.textContent = 'Created By Abdul & Willy under CC licence';
+
+const root = document.getElementById('root');
+
+const main = elementGenerator('main');
+
+for (let i = 1; i <= 12; i += 1) {
+  const meal = elementGenerator('div');
+  const picture = elementGenerator('img', 'image');
+  picture.src = Image;
+  picture.alt = 'space-image';
+
+  const likes = elementGenerator('div', 'likes');
+  const paragraph = elementGenerator('p');
+  paragraph.textContent = `Meal ${i}`;
+
+  const likeCounter = elementGenerator('div', 'like-counter');
+  const heart = elementGenerator('img');
+  heart.src = icon;
+  heart.alt = 'heart-image';
+  const like = elementGenerator('p');
+  like.textContent = 'like';
+
+  const comments = elementGenerator('button', 'commentBtn');
+  comments.textContent = 'comments';
+
+  likeCounter.append(heart, like);
+
+  likes.append(paragraph, likeCounter);
+
+  meal.append(picture, likes, comments);
+
+  main.appendChild(meal);
+}
+
+const createPopup = (meal) => {
+  const popupSection = elementGenerator('section', 'popup-window invisible');
+  let popupMarkup = ` 
+    <small class='close-menu'>X</small>   
+    <div class='blur-background'> 
+      
+      <div class="popup-img-div"><img class="meal-popup-img" src="${Image}" alt="meal" /></div>
+      <div class="popup-details-div">
+      <h1> ${meal.title} </h1>
+      <div class="details-list">
+          <ul>
+          <li>Category: Rice</li>
+          <li>Category: Rice</li>
+          </ul>
+          <ul>
+          <li>Category: Rice</li>
+          <li>Category: Rice</li>
+          </ul>
+      </div>
+    </div>`;
+  popupSection.innerHTML = popupMarkup;
+  popupSection.style.display = 'block';
+  main.style.display = 'none';
+  document.body.appendChild(popupSection);
+  const closePopup = document.querySelector('.close-menu');
+  closePopup.addEventListener('click', () => {
+    popupSection.remove();
+    main.style.display = 'grid';
+  });
+};
+
+const displayPopup = (main) => {
+  const divs = main.children;
+  const mealDetails = { title: '', category: '', price: '', details: '' };
+  for (var i = 0; i < divs.length; i++) {
+    const btn = divs[i].children[2];
+    btn.addEventListener('click', (e) => {
+      const mealTitle =
+        e.target.parentElement.children[1].children[0].textContent;
+      mealDetails.title = mealTitle;
+      createPopup(mealDetails);
+    });
+  }
+};
+
+listOne.appendChild(linkOne);
+listTwo.appendChild(linkTwo);
+listThree.appendChild(linkThree);
+
+uList.append(listOne, listTwo, listThree);
+
+navigation.appendChild(uList);
+
+header.append(logo, navigation);
+
+root.append(header, main, footer);
+
+displayPopup(main);
