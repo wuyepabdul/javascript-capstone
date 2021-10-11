1c50dfcf0880c2f687f2188320d075c1f962a885
wuyepabdul <wuyepabdul@gmail.com>
2021-10-11 15:23:06 +0100

    Add style to popup window section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import './style.css'
+import './style.css';
+import mealImg from './images/meal.jpg';
+
+const createPopup = () => {
+  let popupMarkup = ` 
+  <small class='close-menu'>X</small>   
+  <div class='blur-background'> 
+    
+    <div class="popup-img-div"><img class="meal-popup-img" src="${mealImg}" alt="meal" /></div>
+    <div class="popup-details-div">
+    <h1>Meal title</h1>
+    <div class="details-list">
+        <ul>
+        <li>Category: Rice</li>
+        <li>Category: Rice</li>
+        </ul>
+        <ul>
+        <li>Category: Rice</li>
+        <li>Category: Rice</li>
+        </ul>
+    </div>
+  </div>`;
+
+  const popupSection = document.querySelector('.popup-window');
+  popupSection.innerHTML = popupMarkup;
+  console.log(popupSection);
+};
+
+createPopup();
