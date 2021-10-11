9ddec349eec0b4cee96b8cc69f3a082361587aa0
wuyepabdul <wuyepabdul@gmail.com>
2021-10-11 16:33:42 +0100

    add popup markup in index.js

--- src/index.js
+++ src/index.js
@@ -21,9 +21,33 @@ const createPopup = () => {
     </div>
   </div>`;
 
-  let form = ``
+  const formContainer = `
+  <div class="comments-container">
+  <div class="comments">
+    <h3>Comments (3)</h3>
+  </div>
+  <h3>Add a comment</h3>
+  <form>
+    <div class="input-group">
+      <input type="text" class="nameInput" placeholder="Your name" />
+    </div>
+    <div class="input-group">
+      <textarea
+        name="comment"
+        id="comment"
+        cols="30"
+        rows="10"
+        placeholder="Your insights"
+      ></textarea>
+    </div>
+    <div class="button-div">
+      <button>Comment</button>
+    </div>
+  </form>
+</div>`;
 
   const popupSection = document.querySelector('.popup-window');
   popupSection.innerHTML = popupMarkup;
+//   popupSection.appendChild(formContainer);
 };
-// createPopup();
+createPopup();
